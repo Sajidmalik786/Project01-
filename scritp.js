a7dedb84ddd2908796d9aec8253789e19283f65b
Sajidmalik786 <132780496+Sajidmalik786@users.noreply.github.com>
2024-09-11 10:38:09 +0500

    upload files

---
+++ scritp.js
@@ -0,0 +1,29 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+
+registerBtn.addEventListener('click', ()=>{
+    container.classList.add('active');
+});
+
+loginBtn.addEventListener('click', ()=>{
+    container.classList.remove('active');
+});
+
+
+
+
+// for bg-color change 
+var root = document.querySelector(':root');
+var rootStyle = getComputedStyle(root);
+var bgColor = rootStyle.getPropertyValue('--bg-color');
+var getColor = document.getElementById('chose-color');
+
+
+getColor.addEventListener('input',()=>{
+    colorVal = getColor.value
+    // console.log(colorVal);
+    root.style.setProperty('--bg-color', colorVal);
+
+})
